d9547ecbd877a6a950e0d507ea189ae5b078c50d
zes15 <zes15@my.fsu.edu>
2016-10-02 14:15:15 -0400

    Update InsertEventIter1.sql

--- Source-Code/Database/InsertEventIter1.sql
+++ Source-Code/Database/InsertEventIter1.sql
@@ -7,7 +7,7 @@ CREATE PROCEDURE `InsertEvent` (
 	parmUserID bigint, parmEventName varchar(255), parmLatitude double, parmLongitude double, 
     parmWhen datetime, parmDescription varchar(1024))
 BEGIN
-	INSERT INTO EventTable
+	INSERT INTO eventtable
 		(userID, latitude, longitude, eventName, time, description)
 	VALUES
 		(parmUserId, parmLatitude, parmLongitude, parmEventName, parmWhen, parmDescription);
