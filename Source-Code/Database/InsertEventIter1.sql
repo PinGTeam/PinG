fa598aa1c618771ff50ad63002dee4bb762453c6
tyther9 <tyther9@nettally.com>
2016-10-02 14:02:15 -0400

    Create InsertEventIter1.sql
    For inserting events.

---
+++ Source-Code/Database/InsertEventIter1.sql
@@ -0,0 +1,17 @@
+USE `sirotto_db`;
+DROP procedure IF EXISTS `InsertEvent`;
+
+DELIMITER $$
+USE `sirotto_db`$$
+CREATE PROCEDURE `InsertEvent` (
+	parmUserID bigint, parmEventName varchar(255), parmLatitude double, parmLongitude double, 
+    parmWhen datetime, parmDescription varchar(1024))
+BEGIN
+	INSERT INTO EventTable
+		(userID, latitude, longitude, eventName, time, description)
+	VALUES
+		(parmUserId, parmLatitude, parmLongitude, parmEventName, parmWhen, parmDescription);
+
+END$$
+
+DELIMITER ;
