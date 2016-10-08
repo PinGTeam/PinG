1c839ba313ffab44e73478f76486d9ec843b99ae
monilamb <monica.lambrano@hotmail.com>
2016-10-08 18:18:53 +0000

    Test for calling procedures

---
+++ Source-Code/middle_tier/testcallproc.py
@@ -0,0 +1,46 @@
+#need run flask with following command
+#flask run --host=0.0.0.0
+#need to access http://162.243.15.139:5000/ to get response
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://juan:alfaro@localhost/ping'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://alforo:Be+HQ2Nj@dbsrv2.cs.fsu.edu:3$
+#app.config['MYSQL_DATABASE_USER'] = 'alforo'
+#app.config['MYSQL_DATABASE_PASSWORD'] = 'Be+HQ2Nj'
+#app.config['MYSQL_DATABASE_DB'] = 'alforo_db'
+#app.config['MYSQL_DATABASE_HOST'] = 'dbsrv2.cs.fsu.edu:3306'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://alforo:Be+HQ2Nj@anywheredb.clt1czfvl$
+db = SQLAlchemy(app)
+
+
+@app.route('/')
+def hello_world():
+    str = ""
+    results = users.query.all()
+    for ev in results:
+        str = str + ev.userName + " ,"
+    return str
+
+
+
+class users(db.Model):
+    __tablename__ = 'users'
+    userID = db.Column('userid',db.Integer, primary_key=True)
+    userName = db.Column('userName',db.Unicode, unique=False)
+    firstName = db.Column('firstName',db.Unicode, unique=False)
+    lastName = db.Column('lastName',db.Unicode, unique=False)
+
+
+    #def __init__(self, userName):
+        #self.job_name = job_name
+
+    #def __repr__(self):
+        #return '<Job %r>' % self.job_name
+
+
+args = (123, "Miami Game", 1.2, 1.3, 2, "Watch game")
+cursor.callproc('InsertEvent', args)
