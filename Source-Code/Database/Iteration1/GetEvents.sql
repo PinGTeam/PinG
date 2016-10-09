6cff6a54de05b347fda4fcf8d3e3ac7fa94195ce
tyther9 <tyther9@nettally.com>
2016-10-09 17:52:56 -0400

    Update GetEvents.sql
    Fixed issue with selecting out of bounds events

--- Source-Code/Database/Iteration1/GetEvents.sql
+++ Source-Code/Database/Iteration1/GetEvents.sql
@@ -1,32 +1,36 @@
 USE `ping`;
 DROP procedure IF EXISTS `GetEvents`;
+
 DELIMITER $$
-CREATE PROCEDURE `GetEvents`(topLatitude double,  topLongitude double,  bottomLatitude double,  bottomLongitude double)
+USE `ping`$$
+CREATE DEFINER=`richard`@`localhost` PROCEDURE `GetEvents`(topLatitude double,  topLongitude double,  bottomLatitude double,  bottomLongitude double)
 BEGIN
 
-	SELECT 
-		eventID, FirstName, LastName, latitude, longitude, eventName, time, description
-	FROM 
-		eventtable
-	inner join 
-		users
-	on
-		eventtable.userid = users.userid
-	where
-		(latitude <= topLatitude AND
-        latitude >= bottomLatitude) AND
+SELECT 
+eventID, FirstName, LastName, latitude, longitude, eventName, time, description
+FROM 
+eventtable
+inner join 
+users
+on
+eventtable.userid = users.userid
+where
+(latitude <= @topLatitude AND
+        latitude >= @bottomLatitude) AND
         (
         CASE
-			WHEN 
-			((bottomLongitude <= 180.0000 AND bottomLongitude > 0.0000) AND 
-            (topLongitude > -180.0000 AND topLongitude < 0.0000) AND
-            (longitude <= topLongitude AND longitude > -180.0000) AND
-            (longitude <= 180.0000 and longitude >= bottomLongitude)) THEN 1
-			WHEN
-			((bottomLongitude <= 180.0000  and topLongitude <= 180.0000) AND
-            (longitude >= topLongitude AND longitude <= bottomLongitude)) then 1
-            ELSE
-				0
-		END) = 1;
+WHEN 
+	((@bottomLongitude <= 180.0000 AND @bottomLongitude > 0.0000) AND 
+		(@topLongitude > -180.0000 AND @topLongitude < 0.0000) AND
+            ((longitude <= @topLongitude AND longitude > -180.0000 ) OR
+            (longitude <= 180.0000 and longitude >= @bottomLongitude))) THEN 1
+WHEN
+	((@bottomLongitude < @topLongitude) AND
+	(longitude <= @topLongitude AND longitude >= @bottomLongitude)) then 1
+ELSE
+0
+END) = 1;
 END$$
+
 DELIMITER ;
+
