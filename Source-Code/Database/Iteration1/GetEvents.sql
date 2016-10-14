34e98d783236c715d309a1cb437af65908528480
tyther9 <tyther9@nettally.com>
2016-10-13 20:46:40 -0400

    Update GetEvents.sql
    Fixed @ sign issue

--- Source-Code/Database/Iteration1/GetEvents.sql
+++ Source-Code/Database/Iteration1/GetEvents.sql
@@ -3,7 +3,7 @@ DROP procedure IF EXISTS `GetEvents`;
 
 DELIMITER $$
 USE `ping`$$
-CREATE DEFINER=`richard`@`localhost` PROCEDURE `GetEvents`(topLatitude double,  topLongitude double,  bottomLatitude double,  bottomLongitude double)
+CREATE PROCEDURE `GetEvents`(topLatitude double,  topLongitude double,  bottomLatitude double,  bottomLongitude double)
 BEGIN
 
 SELECT 
@@ -15,18 +15,18 @@ users
 on
 eventtable.userid = users.userid
 where
-(latitude <= @topLatitude AND
-        latitude >= @bottomLatitude) AND
+(latitude <= topLatitude AND
+        latitude >= bottomLatitude) AND
         (
         CASE
 WHEN 
-	((@bottomLongitude <= 180.0000 AND @bottomLongitude > 0.0000) AND 
-		(@topLongitude > -180.0000 AND @topLongitude < 0.0000) AND
-            ((longitude <= @topLongitude AND longitude > -180.0000 ) OR
-            (longitude <= 180.0000 and longitude >= @bottomLongitude))) THEN 1
+	((bottomLongitude <= 180.0000 AND bottomLongitude > 0.0000) AND 
+		(topLongitude > -180.0000 AND topLongitude < 0.0000) AND
+            ((longitude <= topLongitude AND longitude > -180.0000 ) OR
+            (longitude <= 180.0000 and longitude >= bottomLongitude))) THEN 1
 WHEN
-	((@bottomLongitude < @topLongitude) AND
-	(longitude <= @topLongitude AND longitude >= @bottomLongitude)) then 1
+	((bottomLongitude > topLongitude) AND
+	(longitude >= topLongitude AND longitude <= bottomLongitude)) then 1
 ELSE
 0
 END) = 1;
