5a3a83b1802e336f0f49c328ad21f76fd9ab9637
zes15 <zes15@my.fsu.edu>
2016-10-17 09:41:47 -0400

    uploading tablecreation.sql to milestone 3 archive

---
+++ Archive/Best-Team/Source-Code/Back-End-Database/TableCreation.sql
@@ -0,0 +1,22 @@
+CREATE TABLE `ping`.`users` (
+  `userID` BIGINT NOT NULL AUTO_INCREMENT,
+  `userName` VARCHAR(20) NOT NULL,
+  `firstName` VARCHAR(25) NOT NULL,
+  `lastName` VARCHAR(25) NOT NULL,
+  PRIMARY KEY (`userID`));
+  
+  CREATE TABLE `ping`.`eventtable` (
+  `eventID` BIGINT NOT NULL AUTO_INCREMENT,
+  `userID` BIGINT NOT NULL,
+  `latitude` DOUBLE NOT NULL,
+  `longitude` DOUBLE NOT NULL,
+  `eventName` VARCHAR(255) NOT NULL,
+  `time` DATETIME NOT NULL,
+  `description` VARCHAR(1024) NULL,
+  PRIMARY KEY (`eventID`),
+  INDEX `userID_idx` (`userID` ASC),
+  CONSTRAINT `userID`
+    FOREIGN KEY (`userID`)
+    REFERENCES `ping`.`users` (`userID`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION);
