31d4d4c071a37015b737910f9536555ad6d8b082
heller166 <franciscoalfaro2008@gmail.com>
2016-10-28 22:11:14 +0000

    edited insertUsert to return extablished signal for registration errors

--- Source-Code/Database/Iteration1/InsertUser.sql
+++ Source-Code/Database/Iteration1/InsertUser.sql
@@ -3,22 +3,19 @@ DROP procedure IF EXISTS `InsertUser`;
 
 DELIMITER $$
 USE `ping`$$
-CREATE PROCEDURE `InsertUser`(parmUserName varchar(20), parmPassword varchar(60), parmFirstName varchar(25), parmLastName varchar(25), parmEmail varchar(256))
+CREATE PROCEDURE `InsertUser`(parmUserName varchar(20), parmPassword varchar(60), parmFirstName varchar(25), parmLastName varchar(25), parmEmail varchar(320))
 BEGIN
-    IF NOT exists(SELECT * FROM users where userName = parmUserName OR email = parmEmail) THEN
-          insert into users
-        (userName, password, firstName, lastName, email)
-      VALUES
-        (parmUserName,parmPassword, parmFirstName, parmLastName, parmEmail);
-
-        END IF;
-
-        SELECT
-      userID
-        FROM
-      users
-    where
-      userName = parmUserName;
+    IF exists(SELECT * FROM users where email = parmEmail) AND exists(SELECT * FROM users where userName = parmUserName) THEN
+      SELECT -3;
+    ELSEIF exists(SELECT * FROM users where userName = parmUserName) THEN
+      SELECT -2;
+    ELSEIF exists(SELECT * FROM users where email = parmEmail) THEN
+      SELECT -1;
+    ELSE
+      insert into users (userName, password, firstName, lastName, email)
+      VALUES (parmUserName,parmPassword, parmFirstName, parmLastName, parmEmail);
+      SELECT 1;
+    END IF;
 
 END$$
 
