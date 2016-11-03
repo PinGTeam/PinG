1f506cdf29bc715e18146f2842eb853dae1dcdae
heller166 <franciscoalfaro2008@gmail.com>
2016-11-03 21:48:07 +0000

    added two new stored Procedures, GetEvents2: Uses MySQL spacial extensions with test table geom, GetEvents3: Uses formulas to get the bottom and top points needed for GetEvents from only one point that is passed as a parameter. Also added testSpatial.sql and debugMsg.sel, these are temporary files that will be deleted in the futere

---
+++ Source-Code/Database/Iteration1/GetEvents3.sql
@@ -0,0 +1,54 @@
+USE `ping`;
+DROP procedure IF EXISTS `GetEvents3`;
+
+DELIMITER $$
+USE `ping`$$
+CREATE PROCEDURE `GetEvents3`(paramLatitude double,  paramLongitude double)
+BEGIN
+
+DECLARE distMiles INT;
+DECLARE lon2miles DOUBLE;
+DECLARE lat2miles DOUBLE;
+DECLARE topLatitude DOUBLE;
+DECLARE topLongitude DOUBLE;
+DECLARE bottomLatitude DOUBLE;
+DECLARE bottomLongitude DOUBLE;
+
+SET distMiles = 25;
+SET lon2miles = abs(cos(radians(paramLatitude))*69);
+SET lat2miles = 69;
+SET topLongitude = paramLongitude-distMiles/lon2miles;
+SET topLatitude = paramLatitude+distMiles/lat2miles;
+SET bottomLongitude = paramLongitude+distMiles/lon2miles;
+SET bottomLatitude = paramLatitude-distMiles/lat2miles;
+
+SELECT
+eventID, FirstName, LastName, latitude, longitude, eventName, startTime, endTime, description
+FROM
+eventtable
+inner join
+users
+on
+eventtable.userid = users.userid
+where
+(endTime > NOW()) AND
+
+(latitude <= topLatitude AND
+        latitude >= bottomLatitude) AND
+        (
+        CASE
+WHEN(
+(bottomLongitude <= 180.0000 AND bottomLongitude > 0.0000)
+AND
+(topLongitude > -180.0000 AND topLongitude < 0.0000) AND
+            ((longitude <= topLongitude AND longitude > -180.0000 ) OR
+            (longitude <= 180.0000 and longitude >= bottomLongitude))) THEN 1
+WHEN
+  ((bottomLongitude > topLongitude) AND
+  (longitude >= topLongitude AND longitude <= bottomLongitude)) then 1
+ELSE
+0
+END) = 1;
+END$$
+
+DELIMITER ;
